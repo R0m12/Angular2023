3dbf75fd6eb6426d22e086e2f60050a930cb6327
Rhumun <romain221201@gmail.com>
2023-03-21 13:23:48 +0100

    Init

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var studentsRouter = require('./routes/students');
+
+var app = express();
+
+//Intégration de la BDD.
+var connectionString = "mongodb+srv://Romain:Rom221201@angular2023.9uvmbqr.mongodb.net/test";
+var mongoDB = process.env.MONGODB_URI || connectionString;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/students', studentsRouter);
+
+module.exports = app;
